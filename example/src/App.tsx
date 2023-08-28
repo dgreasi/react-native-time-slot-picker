cdf53c1ca7b6b4b17961413e8f687baf7ab8d061
Dgreasidis <dgreasidis+github@gmail.com>
2023-08-28 15:08:09 +0300

    chore: update example app to show selected date slot
    Change-type: patch

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import { useEffect, useState } from 'react';
-import { SafeAreaView, StyleSheet, View } from 'react-native';
+import { SafeAreaView, StatusBar } from 'react-native';
 import {
   IAppointment,
   TimeSlotPicker,
 } from '@dgreasi/react-native-time-slot-picker';
 import { bookedData, dummyAvailableDates } from './data';
+import { SelectedTimeSlot } from './SelectedTimeSlot';
 
 export default function App() {
   const [dateOfAppointment, setDateOfAppointment] =
@@ -16,30 +17,21 @@ export default function App() {
   }, [dateOfAppointment]);
 
   return (
-    <View style={styles.container}>
-      <SafeAreaView>
-        <TimeSlotPicker
-          availableDates={dummyAvailableDates}
-          setDateOfAppointment={setDateOfAppointment}
-          scheduledAppointment={bookedData}
-          // marginTop={24}
-          // datePickerBackgroundColor="#F4CC58"
-          // timeSlotsBackgroundColor="#F4CC58"
-          mainColor="#B4CC51"
-          // timeSlotWidth={116}
-          // dayNamesOverride={greekDayNames}
-          // monthNamesOverride={greekMonthNames}
-        />
-      </SafeAreaView>
-    </View>
+    <SafeAreaView>
+      <StatusBar backgroundColor="transparent" barStyle="dark-content" />
+      <TimeSlotPicker
+        availableDates={dummyAvailableDates}
+        setDateOfAppointment={setDateOfAppointment}
+        scheduledAppointment={bookedData}
+        // marginTop={24}
+        // datePickerBackgroundColor="#F4CC58"
+        // timeSlotsBackgroundColor="#F4CC58"
+        mainColor="#B4CC51"
+        // timeSlotWidth={116}
+        // dayNamesOverride={greekDayNames}
+        // monthNamesOverride={greekMonthNames}
+      />
+      <SelectedTimeSlot dateOfAppointment={dateOfAppointment} />
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'flex-start',
-    justifyContent: 'flex-start',
-    flexDirection: 'column',
-  },
-});
